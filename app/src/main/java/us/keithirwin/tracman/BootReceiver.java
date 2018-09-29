3c215b460ca471f847944c2c065d87794f4e18da
Keith Irwin <mail@keithirwin.us>
2018-09-28 18:34:42 -0600

    Copied boot receiver from master

--- app/src/main/java/us/keithirwin/tracman/BootReceiver.java
+++ app/src/main/java/us/keithirwin/tracman/BootReceiver.java
@@ -3,13 +3,17 @@ package us.keithirwin.tracman;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        // Starts location service on boot
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        if (prefs.getBoolean("pref_start_boot", false)) {
+            context.startService(new Intent(context, LocationService.class));
+        }
     }
 }
